9ebe9412b5df6f0e96d7069fc85a438a2898a633
Brian Stocking <brianm.stocking@gmail.com>
2015-02-19 14:57:17 -0700

    added some style

--- js/timeDirective.js
+++ js/timeDirective.js
@@ -4,7 +4,7 @@ app.directive('showTime', function () {
 
   return {
     restrict: 'E',
-    template: '<div> The current time is {{time | date: "short"}} </div>',
+    template: '<div class="texts"> The current time is {{time | date: "short"}} </div>',
     link: function (scope, element, attrs) {
       var currentTime = new Date();
       scope.time = currentTime;
